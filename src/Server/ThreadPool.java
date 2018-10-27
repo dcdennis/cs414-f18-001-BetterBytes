934f4a6ab14ce0506b9fd46fa9935216434bc94f
John Pokorski <jhpokorski@gmail.com>
2018-10-27 13:28:26 -0600

    Add files via upload
    Now with runnable code!

---
+++ src/Server/ThreadPool.java
@@ -0,0 +1,24 @@
+package Server;
+
+import java.util.ArrayList;
+
+public class ThreadPool 
+{
+	private ArrayList<WorkerThread> threadList;
+	public ThreadPool(int threadCount)
+	{
+		threadList = new ArrayList<WorkerThread>();
+		for(int i = 0; i < threadCount; i++)
+			threadList.add(new WorkerThread(i));
+		for(WorkerThread thread : threadList)
+			thread.start();
+	}
+	public void terminate()
+	{
+		for(WorkerThread i : threadList)
+		{
+			i.terminate();
+			i.interrupt();
+		}
+	}
+}

7876ff0ec39d06713445c707e46ee77d9189299e
Devin Dennis <31259185+dcdennis@users.noreply.github.com>
2018-11-09 13:54:53 -0700

    Adds ServerData Class -- check comments on usage

---
+++ src/edu/colostate/cs/cs414/betterbytes/p3/server/ServerData.java
@@ -0,0 +1,35 @@
+package edu.colostate.cs.cs414.betterbytes.p3.server;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import edu.colostate.cs.cs414.betterbytes.p3.user.Account;
+import java.nio.channels.SocketChannel;
+
+// This class allows access to an active account's socket channel
+// The server can now write to a specified client
+// TODO: when a client logins in we need to create an instance of their account from the db
+//		 when the Account class has been serialized we can hook it to their SocketChannel
+public class ServerData {
+
+	// FIELDS
+	private HashMap<Account, SocketChannel> AccountChannels = new HashMap< Account, SocketChannel>();	
+	
+	// CTOR INSTANCE
+	private static final ServerData instance = new ServerData();
+
+	public static ServerData getInstance() {
+		return instance;
+	}	
+	
+	// ACCESSORS
+	public SocketChannel getChannel(Account account) {
+		return this.AccountChannels.get(account);
+	}
+	
+	//MUTATORS	
+	public void addAccountChannelPair(Account account, SocketChannel channel) {
+		this.AccountChannels.put(account, channel);
+	}
+	
+}

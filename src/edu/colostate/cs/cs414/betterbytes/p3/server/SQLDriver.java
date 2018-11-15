1874fba947d22b692868063537389f60808169b7
Carson-Tunnell <carsontunnell@hotmail.com>
2018-11-15 13:25:43 -0700

    Added SQL Game feautes
    SQL not supports creating games and deleting games.
    It was also made more robust so it searches for each combination in the database since we do not specify which is player 1 or player 2

--- src/edu/colostate/cs/cs414/betterbytes/p3/server/SQLDriver.java
+++ src/edu/colostate/cs/cs414/betterbytes/p3/server/SQLDriver.java
@@ -7,7 +7,9 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
+import edu.colostate.cs.cs414.betterbytes.p3.game.Game;
 import edu.colostate.cs.cs414.betterbytes.p3.user.Account;
+import edu.colostate.cs.cs414.betterbytes.p3.user.Player;
 
 public class SQLDriver {
 
@@ -69,6 +71,130 @@ public class SQLDriver {
 			return true;
 		}
 	}// End Methods
+	
+	
+	
+	public void addGame(Player player1, Player player2, String state)
+	{
+		String p1 = player1.getAccount().getUsername();
+		String p2 = player2.getAccount().getUsername();
+		addGame(p1,p2,state);	
+	}
+	
+	
+	public void addGame(Account player1, Account player2, String state)
+	{
+		String p1 = player1.getUsername();
+		String p2 = player2.getUsername();
+		addGame(p1,p2,state);
+	}
+	//TODO
+	// state Must be changed to a Game
+	// String for testing purposes
+	private void addGame (String p1, String p2, String state)
+	{		
+		String query = "INSERT INTO betterbytes.game (player1,player2,state) "
+				+ "VALUE('" + p1 + "' , '" + p2 + "' , '" + state + "');";
+		runQuery(query);		
+	}
+	
+	public boolean deleteGame(Player player1, Player player2)
+	{
+		String p1 = player1.getAccount().getUsername();
+		String p2 = player2.getAccount().getUsername();
+		return deleteGame(p1,p2);	
+	}
+	
+	
+	public boolean deleteGame(Account player1, Account player2)
+	{
+		String p1 = player1.getUsername();
+		String p2 = player2.getUsername();
+		return deleteGame(p1,p2);
+	}
+	
+	private boolean deleteGame(String p1, String p2)
+	{
+		String query = "DELETE FROM betterbytes.game WHERE `player1` LIKE '" + p1 + "' and `player2` LIKE '"
+				+ p2 + "';";
+		runQuery(query);
+		
+		query = "DELETE FROM betterbytes.game WHERE `player1` LIKE '" + p2 + "' and `player2` LIKE '"
+				+ p1 + "';";
+		runQuery(query);
+		
+		String result = getGame(p1,p2);
+		
+		if(result == null || result == "null")
+			return true;
+		return false;
+	}
+	
+	public String getGame(Player player1,Player player2)
+	{
+		String p1 = player1.getAccount().getUsername();
+		String p2 = player2.getAccount().getUsername();
+		String result = getGame(p1,p2);
+		
+		return result;
+	}
+	
+	public String getGame(Account player1,Account player2)
+	{
+		String p1 = player1.getUsername();
+		String p2 = player2.getUsername();
+		String result = getGame(p1,p2);
+		
+		return result;
+	}
+	
+	
+	private String [] queryGame(String query)
+	{
+		String[] results = new String[3];
+		try {
+			Statement st = database.createStatement();
+			try {
+				ResultSet result = st.executeQuery(query);
+				try {
+					ResultSetMetaData metaData = result.getMetaData();
+					int colCount = metaData.getColumnCount();
+					while (result.next()) {
+						results[0] = result.getString(1);
+						results[1] = result.getString(2);
+						results[2] = result.getString(3);
+					}
+				} finally {
+					result.close();
+				}
+			} finally {
+				st.close();
+			}
+		} catch (Exception e) {
+			System.err.printf("Exception in getGame(): ");
+			System.err.println(e.getMessage());
+		}		
+		return results;		
+	}
+	
+	
+	//Checks for the player being in either player1 or player2 in the database
+	private String getGame(String p1,String p2)
+	{		
+		String query = "SELECT * FROM betterbytes.game WHERE `player1` LIKE \"" + p1 +
+				       "\" and `player2` LIKE \"" + p2 + "\";";			
+		String [] results = queryGame(query);
+	
+		if(results[2] == null || results[2].equals("null"))
+		{
+			query = "SELECT * FROM betterbytes.game WHERE `player1` LIKE \"" + p2 +
+				       "\" and `player2` LIKE \"" + p1 + "\";";			
+			results = queryGame(query);			
+		}	
+		
+		return results[2];
+	}
+
 
 	public boolean deleteUser(String username, String password) {
 		String query = "DELETE FROM betterbytes.users WHERE `username` LIKE '" + username + "' and `password` LIKE '"

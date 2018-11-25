ce7a5e058a1622a999a36de0ee461ed4cc49c082
jhpok <jhpok@10.253.137.166>
2018-11-25 11:09:51 -0700

    Added new constructor to account, can not make moves on a game, completed checking if game is ended

--- src/edu/colostate/cs/cs414/betterbytes/p3/utilities/RulesEngine.java
+++ src/edu/colostate/cs/cs414/betterbytes/p3/utilities/RulesEngine.java
@@ -6,6 +6,7 @@ import edu.colostate.cs.cs414.betterbytes.p3.game.Cell;
 import edu.colostate.cs.cs414.betterbytes.p3.game.Game;
 import edu.colostate.cs.cs414.betterbytes.p3.game.GameResult;
 import edu.colostate.cs.cs414.betterbytes.p3.game.Move;
+import edu.colostate.cs.cs414.betterbytes.p3.ui.PieceType;
 
 public class RulesEngine {
 	private static RulesEngine instance = new RulesEngine();
@@ -19,27 +20,62 @@ public class RulesEngine {
 		
 		//CASE: King has been captured
 		boolean kingFound = false;
+		edu.colostate.cs.cs414.betterbytes.p3.ui.Cell kingLoc = null;
 		for(int i = 0; i < 11; i++)
 		{
 			for(int j=0; j< 11; j++)
 			{
-				Cell checkLoc = game.getCell(i,j);
-				if(checkLoc.isOccupied() && checkLoc.getPiece().isKing())
+				edu.colostate.cs.cs414.betterbytes.p3.ui.Cell checkLoc = game.getCell(i,j);
+				if(checkLoc.hasPiece() && checkLoc.getPiece().getType() == PieceType.KING)
+				{
 					kingFound = true;
+					kingLoc = checkLoc;
+				}
+			
 			}
 		}
 		if(!kingFound)
 			return GameResult.BLACK;
 		
 		//CASE: King has escaped
-		if(	game.getCell(0, 0).isOccupied() || game.getCell(0,10).isOccupied() ||
-				game.getCell(10, 0).isOccupied() || game.getCell(10, 10).isOccupied())
+		if(	game.getCell(0, 0).hasPiece() || game.getCell(0,10).hasPiece() ||
+				game.getCell(10, 0).hasPiece() || game.getCell(10, 10).hasPiece())
 			return GameResult.WHITE;
 		
 		//CASE: King is on an edge, surrounded by black pieces, and there are no other white pieces
 		
-		//TODO
-		
+		if(kingLoc.getX() == 0)
+		{
+			if(	game.getCell(1, kingLoc.getY()).hasPiece() && !game.getCell(1, kingLoc.getY()).getPiece().isWhite() &&
+				game.getCell(0, kingLoc.getY()+1).hasPiece() && !game.getCell(1, kingLoc.getY()).getPiece().isWhite() &&
+				game.getCell(0, kingLoc.getY()-1).hasPiece() && !game.getCell(1, kingLoc.getY()).getPiece().isWhite() &&
+				this.kingAlone(game))
+				return GameResult.BLACK;
+		}
+		if(kingLoc.getX() == 10)
+		{
+			if(	game.getCell(9, kingLoc.getY()).hasPiece() && !game.getCell(1, kingLoc.getY()).getPiece().isWhite() &&
+					game.getCell(10, kingLoc.getY()+1).hasPiece() && !game.getCell(1, kingLoc.getY()).getPiece().isWhite() &&
+					game.getCell(10, kingLoc.getY()-1).hasPiece() && !game.getCell(1, kingLoc.getY()).getPiece().isWhite() &&
+					this.kingAlone(game))
+					return GameResult.BLACK;
+		}
+		if(kingLoc.getY() == 0)
+		{
+			if(	game.getCell(kingLoc.getX(), 1).hasPiece() && !game.getCell(kingLoc.getX(), 1).getPiece().isWhite() &&
+					game.getCell(kingLoc.getX()+1, 0).hasPiece() && !game.getCell(kingLoc.getX()+1, 0).getPiece().isWhite() &&
+					game.getCell(kingLoc.getX()-1, 0).hasPiece() && !game.getCell(kingLoc.getX()-1, 0).getPiece().isWhite() &&
+					this.kingAlone(game))
+					return GameResult.BLACK;
+		}
+		if(kingLoc.getY() == 10)
+		{
+			if(	game.getCell(kingLoc.getX(), 9).hasPiece() && !game.getCell(kingLoc.getX(), 9).getPiece().isWhite() &&
+					game.getCell(kingLoc.getX()+1, 10).hasPiece() && !game.getCell(kingLoc.getX()+1, 10).getPiece().isWhite() &&
+					game.getCell(kingLoc.getX()-1, 10).hasPiece() && !game.getCell(kingLoc.getX()-1, 10).getPiece().isWhite() &&
+					this.kingAlone(game))
+					return GameResult.BLACK;
+		}
 		//CASE: Move pattern repetition
 		List<Move> moveHistory = game.getMoves();
 		if(moveHistory.size() > 12)
@@ -50,34 +86,20 @@ public class RulesEngine {
 		}
 		return GameResult.CONTINUE;		
 	}
-	/**
-	 * checkTerminal(State,moveHistory)
-{
-    //King has been capturd
-    kingFound = false
-    for(i = 0; i < 11; 1++)
-        for(j=0; j< 11; j++)
-            if( state.location(i,j).contains != null && state.location(i,j).contains.type == king)
-                kingFound = true
-    if(!kingFound)
-        return black
-    
-    //King has escaped
-    if(state.location(0,0).contains != null || state.location(0,10).contains != null || state.location(10,0).contains != null state.location(10,10).contains != null)
-        return white
-        
-    //White only has a king, which can not move
-    ?????
-    
-    //Repeated move pattern
-    if(moveHistory.length > 12)
-    {
-        movePattern = moveHistory.get(moveHistory.length - 3, moveHistory.length - 0)
-        if(moveHistory.get(moveHistory.length - 7, moveHistory - 4).equals(movePattern) && moveHistory.get(moveHistory.length - 11, moveHistory - 8).equals(movePattern))
-            return draw
-    }
-    
-}
-
-	 */
+	private boolean kingAlone(Game game) {
+		boolean nonKingFound = false;
+		for(int i = 0; i < 11; i++)
+		{
+			for(int j=0; j< 11; j++)
+			{
+				edu.colostate.cs.cs414.betterbytes.p3.ui.Cell checkLoc = game.getCell(i,j);
+				if(checkLoc.hasPiece() && checkLoc.getPiece().getType() == PieceType.ROOK)
+				{
+					nonKingFound = true;
+				}
+			
+			}
+		}
+		return nonKingFound;
+	}
 }

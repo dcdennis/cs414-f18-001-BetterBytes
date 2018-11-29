cfcb6754d4da680793698a6b3ad790297949a2bc
LemonOnRye <redhayden@gmail.com>
2018-11-29 11:56:24 -0700

    javadoc for Server ServerData and SQLDriver

--- src/edu/colostate/cs/cs414/betterbytes/p3/server/SQLDriver.java
+++ src/edu/colostate/cs/cs414/betterbytes/p3/server/SQLDriver.java
@@ -14,6 +14,10 @@ import edu.colostate.cs.cs414.betterbytes.p3.user.Account;
 import edu.colostate.cs.cs414.betterbytes.p3.user.Player;
 import edu.colostate.cs.cs414.betterbytes.p3.utilities.Serializer;
 
+/**
+ * Handles SQL DB access and data storage. Defaults to faure.cs.colostate.edu/betterbytes
+ * @version 1.0
+ */
 public class SQLDriver {
 
 	// Code to add the cases needed for the testing to work
@@ -52,6 +56,10 @@ public class SQLDriver {
 
 	private static final SQLDriver instance = new SQLDriver();
 
+	/**
+	 * Gets the singleton instance object
+	 * @return the instance of the SQLDriver
+	 */
 	public static SQLDriver getInstance() {
 		return instance;
 	}
@@ -60,6 +68,12 @@ public class SQLDriver {
 		openConnection(url, user, pass);
 	}
 
+	/**
+	 * Opens a connect with the database based on the url, user, and their password
+	 * @param url url of the database
+	 * @param User Username credential
+	 * @param pass Password credential
+	 */
 	private void openConnection(String url, String User, String pass) {
 		String driver = "com.mysql.cj.jdbc.Driver";
 		try {
@@ -71,6 +85,13 @@ public class SQLDriver {
 		}
 	}
 
+	/**
+	 * Makes a query for the database to update the username and password fields for a given account.
+	 * Username and password fields must be correct, though inputs are not sanitized.
+	 * @param username Username to set
+	 * @param password Password to set
+	 * @param acc Account to set the fields in
+	 */
 	public void setAccount(String username, String password, Account acc) {
 		String query = "UPDATE betterbytes.users SET `username`=\"" + username + "\", " + "`password`=\"" + password
 				+ "\",`account`=\"" + new String(Serializer.serialize(acc)) + "\" ";
@@ -79,6 +100,10 @@ public class SQLDriver {
 		runQuery(query);
 	}
 
+	/**
+	 * Makes a query to update the account in the database. Gets password and username fields from the account.
+	 * @param acc Account to set
+	 */
 	public void setAccount(Account acc) {
 		String query = "UPDATE betterbytes.users SET `username`=\"" + acc.getUsername() + "\", " + "`password`=\""
 				+ acc.getPassword() + "\",`account`=\"" + new String(Serializer.serialize(acc)) + "\" ";
@@ -87,8 +112,12 @@ public class SQLDriver {
 		runQuery(query);
 	}
 
-	// Gets the account from a given username and password
-	// Returns the Account object
+	/**
+	 * Gets the account from a given username and password. Counts a login query.
+	 * @param username Username of the account
+	 * @param password Password of the account
+	 * @return the account object based on the username and password
+	 */
 	public Account getAccount(String username, String password) {
 		String[] results = loginQuery(username, password);
 		Account received = Serializer.deserializeAccount(results[2].getBytes());
@@ -96,6 +125,11 @@ public class SQLDriver {
 		return received;
 	}
 
+	/**
+	 * Gets the account from a given username.
+	 * @param username Username of the account
+	 * @return the account object based on the username
+	 */
 	public Account getAccount(String username) {
 		String query = "Select * FROM betterbytes.users";
 		query += " WHERE `username`=\"" + username + "\";";
@@ -109,6 +143,13 @@ public class SQLDriver {
 	// if they do not then we add them to the database
 	// needs work to determine if name already exists.
 	// will work with GUI team to determine return values
+
+	/**
+	 * Checks if a user already exists. If they do not then are added to the database.
+	 * @param username Username to add
+	 * @param password Password to add linked to the username
+	 * @return true if a user was added, false otherwise
+	 */
 	public boolean addUser(String username, String password) {
 		String result[] = loginQuery(username, password);
 		if (result[0] != null) {
@@ -121,6 +162,11 @@ public class SQLDriver {
 		}
 	}// End Methods
 
+	/**
+	 * Checks if an account already exists. If they do not then they are added to the database.
+	 * @param acc Account to add
+	 * @return true if they account was added, false otherwise
+	 */
 	public boolean addUser(Account acc) {
 		String result[] = loginQuery(acc.getUsername(), acc.getPassword());
 		if (result[0] != null) {
@@ -133,36 +179,72 @@ public class SQLDriver {
 		}
 	}// End Methods
 
+	/**
+	 * Adds a game to the database between two players and a given state
+	 * @param player1 Player 1
+	 * @param player2 Player 2
+	 * @param state Game object
+	 */
 	public void addGame(Player player1, Player player2, Game state) {
 		String p1 = player1.getAccount().getUsername();
 		String p2 = player2.getAccount().getUsername();
 		addGame(p1, p2, state);
 	}
 
+	/**
+	 * Adds a game to the database between two accounts and a given state
+	 * @param player1 Player 1's account
+	 * @param player2 Player 2's account
+	 * @param state Game object
+	 */
 	public void addGame(Account player1, Account player2, Game state) {
 		String p1 = player1.getUsername();
 		String p2 = player2.getUsername();
 		addGame(p1, p2, state);
 	}
 
+	/**
+	 * Adds a game to the database between two players as strings and a given state
+	 * @param p1 Player 1 as a string
+	 * @param p2 Player 2 as a string
+	 * @param state Game object
+	 */
 	void addGame(String p1, String p2, Game state) {
 		String query = "INSERT INTO betterbytes.game (player1,player2,state) " + "VALUE('" + p1 + "' , '" + p2 + "' , '"
 				+ new String(Serializer.serialize(state)) + "');";
 		runQuery(query);
 	}
 
+	/**
+	 * Delete a game between two players
+	 * @param player1 Player 1
+	 * @param player2 Player 2
+	 * @return true if the game was deleted, false otherwise
+	 */
 	public boolean deleteGame(Player player1, Player player2) {
 		String p1 = player1.getAccount().getUsername();
 		String p2 = player2.getAccount().getUsername();
 		return deleteGame(p1, p2);
 	}
 
+	/**
+	 * Delete a game between two player's accounts
+	 * @param player1 Player 1's account
+	 * @param player2 Player 2's account
+	 * @return true if the game was deleted, false otherwise
+	 */
 	public boolean deleteGame(Account player1, Account player2) {
 		String p1 = player1.getUsername();
 		String p2 = player2.getUsername();
 		return deleteGame(p1, p2);
 	}
 
+	/**
+	 * Delete a game between two players as strings
+	 * @param p1 Player 1 as a string
+	 * @param p2 Player 2 as a string
+	 * @return true if the game was deleted, false otherwise
+	 */
 	private boolean deleteGame(String p1, String p2) {
 		String query = "DELETE FROM betterbytes.game WHERE `player1` LIKE '" + p1 + "' and `player2` LIKE '" + p2
 				+ "';";
@@ -178,6 +260,12 @@ public class SQLDriver {
 		return false;
 	}
 
+	/**
+	 * Gets a current game between two players
+	 * @param player1 Player 1
+	 * @param player2 Player 2
+	 * @return true if the game was retrieved, false otherwise
+	 */
 	public Game getGame(Player player1, Player player2) {
 		String p1 = player1.getAccount().getUsername();
 		String p2 = player2.getAccount().getUsername();
@@ -186,6 +274,12 @@ public class SQLDriver {
 		return result;
 	}
 
+	/**
+	 * Gets a current game between two players' accounts
+	 * @param player1 Player 1's account
+	 * @param player2 Player 2's account
+	 * @return true if the game was retrieved, false otherwise
+	 */
 	public Game getGame(Account player1, Account player2) {
 		String p1 = player1.getUsername();
 		String p2 = player2.getUsername();
@@ -194,6 +288,11 @@ public class SQLDriver {
 		return result;
 	}
 
+	/**
+	 * Executes a Query and returns the results
+	 * @param query database Query
+	 * @return the size 3 results array
+	 */
 	private String[] queryGame(String query) {
 		String[] results = new String[3];
 		try {
@@ -222,6 +321,13 @@ public class SQLDriver {
 	}
 
 	// Checks for the player being in either player1 or player2 in the database
+
+	/**
+	 * Gets the game being played between two players as strings
+	 * @param p1 Player 1 as a string
+	 * @param p2 Player 2 as a string
+	 * @return the Game object being played
+	 */
 	private Game getGame(String p1, String p2) {
 		String query = "SELECT * FROM betterbytes.game WHERE `player1` LIKE \"" + p1 + "\" and `player2` LIKE \"" + p2
 				+ "\";";
@@ -236,6 +342,12 @@ public class SQLDriver {
 		return Serializer.deserializeGame(results[2].getBytes());
 	}
 
+	/**
+	 * Deletes a user from the database based on username and password
+	 * @param username Username of the user
+	 * @param password Password of the user
+	 * @return true if the user was deleted, false otherwise
+	 */
 	public boolean deleteUser(String username, String password) {
 		String query = "DELETE FROM betterbytes.users WHERE `username` LIKE '" + username + "' and `password` LIKE '"
 				+ password + "';";
@@ -249,6 +361,11 @@ public class SQLDriver {
 			return false;
 	}// End Method
 
+	/**
+	 * Deletes a user from the database based on an account
+	 * @param acc Account of the user
+	 * @return true if the user was deleted, false otherwise
+	 */
 	public boolean deleteUser(Account acc) {
 		String query = "DELETE FROM betterbytes.users WHERE `username` LIKE '" + acc.getUsername()
 				+ "' and `password` LIKE '" + acc.getPassword() + "';";
@@ -262,6 +379,10 @@ public class SQLDriver {
 			return false;
 	}// End Method
 
+	/**
+	 * Executes a query without any return
+	 * @param query Query to run
+	 */
 	private void runQuery(String query) {
 		try {
 			Statement st = database.createStatement();
@@ -277,6 +398,12 @@ public class SQLDriver {
 	}
 
 	// Can be changed to Arraylist if needed to be more specific.
+
+	/**
+	 * Executes a query and returns the results of the query
+	 * @param query Query to run
+	 * @return results of the query
+	 */
 	private String[] runQueryRes(String query) {
 		String[] results = new String[3];
 		try {
@@ -304,6 +431,11 @@ public class SQLDriver {
 		return results;
 	}
 
+	/**
+	 * Executes a query and returns a list of results
+	 * @param query Query to run
+	 * @return List of the query results
+	 */
 	private List<String> getGamesQuery(String query) {
 		List<String> results = new ArrayList<String>();
 		try {
@@ -329,6 +461,12 @@ public class SQLDriver {
 		return results;
 	}
 
+	/**
+	 * Check if a username/password pair is valid
+	 * @param username Username
+	 * @param password Password
+	 * @return true if the username password pair is valid, false otherwise
+	 */
 	public boolean checkLogin(String username, String password) {
 		String result[] = loginQuery(username, password);
 		if (result[1] == null || !(result[1].equals(password)))
@@ -340,6 +478,12 @@ public class SQLDriver {
 
 	}// End method
 
+	/**
+	 * Executes a long query based on the username and password pair
+	 * @param username Username
+	 * @param password Password
+	 * @return results of the long in query
+	 */
 	public String[] loginQuery(String username, String password) {
 		String[] results = new String[3];
 		String query = "SELECT * FROM betterbytes.users WHERE `username` LIKE '" + username + "' AND `password` LIKE '"
--- src/edu/colostate/cs/cs414/betterbytes/p3/server/ServerData.java
+++ src/edu/colostate/cs/cs414/betterbytes/p3/server/ServerData.java
@@ -9,6 +9,11 @@ import edu.colostate.cs.cs414.betterbytes.p3.user.Account;
 // The server can now write to a specified client
 // TODO: when a client logins in we need to create an instance of their account from the db
 //		 when the Account class has been serialized we can hook it to their SocketChannel
+
+/**
+ * Allows access to account's socket channel, allowing for writing to a specified client.
+ * @version 1.0
+ */
 public class ServerData {
 
 	// FIELDS
@@ -17,16 +22,32 @@ public class ServerData {
 	// CTOR INSTANCE
 	private static final ServerData instance = new ServerData();
 
+	/**
+	 * Gets the singleton instance object
+	 * @return the instance of the server data
+	 */
 	public static ServerData getInstance() {
 		return instance;
 	}
 
 	// ACCESSORS
+
+	/**
+	 * Gets an accounts current communication channel
+	 * @param account Active account in the database
+	 * @return the current channel linked to that account
+	 */
 	public SocketChannel getChannel(Account account) {
 		return this.AccountChannels.get(account);
 	}
 
 	// MUTATORS
+
+	/**
+	 * Adds a mapping between the given account and channel
+	 * @param account Account to pair
+	 * @param channel Channel to pair
+	 */
 	public void addAccountChannelPair(Account account, SocketChannel channel) {
 		this.AccountChannels.put(account, channel);
 	}

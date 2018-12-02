0cf120c0f28c6e738d5568cd6a9c1a7af75c09df
jhpok <jhpok@10.253.137.166>
2018-12-02 10:42:31 -0700

    Poking around in AIClient, I dont think it fixed anything
    ITs a bit prettyier and I want everythin to be in synch

--- src/edu/colostate/cs/cs414/betterbytes/p3/client/AIClient.java
+++ src/edu/colostate/cs/cs414/betterbytes/p3/client/AIClient.java
@@ -25,18 +25,16 @@ import edu.colostate.cs.cs414.betterbytes.p3.wireforms.SubmitMove;
 import edu.colostate.cs.cs414.betterbytes.p3.wireforms.UserLogon;
 import edu.colostate.cs.cs414.betterbytes.p3.wireforms.UserLogonResponse;
 
-
 public class AIClient {
-	
+
 	static Path currentRelativePath = Paths.get("");
-	static String path = currentRelativePath.toAbsolutePath().toString() + "/edu/colostate/cs/cs414/betterbytes/p3/client/";
+	static String path = currentRelativePath.toAbsolutePath().toString()
+			+ "/edu/colostate/cs/cs414/betterbytes/p3/client/";
 	private static final String AIScriptPath = "Python " + path + "HnefataflAI.py";
 
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
-	IllegalAccessException, UnsupportedLookAndFeelException, IOException, InterruptedException 
-	{
-		try 
-		{
+			IllegalAccessException, UnsupportedLookAndFeelException, IOException, InterruptedException {
+		try {
 			System.out.println("Getting client connection");
 			ClientConnection connection = ClientConnection.getInstance();
 			System.out.println("Setting up connection");
@@ -44,66 +42,66 @@ public class AIClient {
 			connection.start();
 			System.out.println("connection started");
 			Thread.sleep(1000);
-			//UserLogonResponse loginResponse = (UserLogonResponse) connection.send(new UserLogon("AIUSER","AILOGON"));
-			
-			Message response = connection.send(new UserLogon("AIUSER","AILOGON"));			
+			// UserLogonResponse loginResponse = (UserLogonResponse) connection.send(new
+			// UserLogon("AIUSER","AILOGON"));
+
+			Message response = connection.send(new UserLogon("AIUSER", "AILOGON"));
 			Account aiAccount = ((UserLogonResponse) response).getAcc();
-			
+
 			System.out.println("Logged on");
-			//Account aiAccount = loginResponse.getAcc();
-			while(true)
-			{
-				//Update the account object
-				RecordsRequestResponse recordsResponse = (RecordsRequestResponse) connection.send(new RecordsRequest(aiAccount.getUsername()));
+			// Account aiAccount = loginResponse.getAcc();
+			while (true) {
+				// Update the account object
+				RecordsRequestResponse recordsResponse = (RecordsRequestResponse) connection
+						.send(new RecordsRequest(aiAccount.getUsername()));
 				aiAccount = recordsResponse.getAccount();
 				List<Game> games = recordsResponse.getGames();
-				//Accept all invites
+				// Accept all invites
 				List<Invitation> invites = aiAccount.getInvites();
-				if(invites != null)
-					for(Invitation invite : invites)
-					{
+				if (invites != null)
+					for (Invitation invite : invites) {
 						connection.send(new RespondToInvitation(invite));
 						Thread.sleep(100);
 					}
-				
-				//Act on all games
-				for(Game game : games)
-				{
-					if(game.getResult() == GameResult.CONTINUE && game.getTurn().getAccount().getUsername().equals("AIUSER"));
-					{
-						//Build the input strings for the python script
-						String board = game.dump();
-						String color;
-						if(game.getAttacker().getAccount().getUsername().equals("AIUSER"))
-							color = "white";
-						else
-							color = "black";
-							//Write the input strings to a file												
-						System.out.println("Path: " + path);
-						BufferedWriter writer = new BufferedWriter(new FileWriter( path + "incomingGame.txt"));											
-						writer.write(board + "\n" + color);
-						writer.close();
-						//Run the AI Script, wait for it to complete
-						System.out.println("Running: " + AIScriptPath);
-						Process AIScript = Runtime.getRuntime().exec(AIScriptPath);
-						AIScript.waitFor();
-						String scriptOutput = "";
-						try{
-							Thread.sleep(1000);
-							scriptOutput = new String(Files.readAllBytes(Paths.get(path + "outgoingGame.txt")));
-							Game gameUpdate = new Game(game,scriptOutput);
-							connection.send(new SubmitMove(gameUpdate));
-							
-							}catch(IOException e){
+
+				// Act on all games
+				if (games != null) {
+					for (Game game : games) {
+						if (game.getResult() == GameResult.CONTINUE
+								&& game.getTurn().getAccount().getUsername().equals("AIUSER"));
+						{
+							// Build the input strings for the python script
+							String board = game.dump();
+							String color = game.getTurn().getColor();
+
+							// Write the input strings to a file
+							System.out.println("Path: " + path);
+							BufferedWriter writer = new BufferedWriter(new FileWriter(path + "incomingGame.txt"));
+							writer.write(board + "\n" + color);
+							writer.close();
+
+							// Run the AI Script, wait for it to complete
+							System.out.println("Running: " + AIScriptPath);
+							Process AIScript = Runtime.getRuntime().exec(AIScriptPath);
+							AIScript.waitFor();
+
+							// Read in the output file, create a new Game object, and send it to the server
+							String scriptOutput = "";
+							try {
+								scriptOutput = new String(Files.readAllBytes(Paths.get(path + "outgoingGame.txt")));
+								Game gameUpdate = new Game(game, scriptOutput);
+								connection.send(new SubmitMove(gameUpdate));
+								Thread.sleep(100);
+
+							} catch (IOException e) {
 								e.printStackTrace();
+							}
 						}
 					}
 				}
 			}
-		} 
-		catch (UnknownHostException e) 
-		{
-		e.printStackTrace();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
 		}
 
 	}

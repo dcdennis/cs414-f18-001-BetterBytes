3fef6ba56b9ab2a313e3cf4898321942c5bdbda2
LemonOnRye <redhayden@gmail.com>
2018-12-09 10:38:50 -0700

    Added more doc and one small change to RulesEngine.java

--- src/edu/colostate/cs/cs414/betterbytes/p4/server/utilities/RulesEngine.java
+++ src/edu/colostate/cs/cs414/betterbytes/p4/server/utilities/RulesEngine.java
@@ -11,16 +11,31 @@ import edu.colostate.cs.cs414.betterbytes.p4.hnefatafl.game.Move;
 import edu.colostate.cs.cs414.betterbytes.p4.server.wireforms.RecordsRequest;
 import edu.colostate.cs.cs414.betterbytes.p4.user.Player;
 
+/**
+ * RulesEngine class. Handles turn by turn game logic.
+ * Note: Some variables have the full import path, but this doesn't appear necessary since we already imported.
+ * As to not break the code on the last day, I'm not going to change it -Hayden.
+ * @version 1.1
+ */
 public class RulesEngine {
 	private static RulesEngine instance = new RulesEngine();
 
 	private RulesEngine() {
 	}
 
+	/**
+	 * Gets the singleton instance object
+	 * @return instance object
+	 */
 	public static RulesEngine getInstance() {
 		return instance; 
 	}
 
+	/**
+	 * Figure out if a given game has ended
+	 * @param game Game object
+	 * @return True if the game has ended, false otherwise
+	 */
 	public GameResult gameHasEnded(Game game) {
 
 		// CASE: King has been captured
@@ -36,6 +51,7 @@ public class RulesEngine {
 
 			}
 		}
+		//No king on the board, so the Black player must have won
 		if (!kingFound)
 		{	
 			System.out.println("1"); 
@@ -102,24 +118,36 @@ public class RulesEngine {
 		return GameResult.CONTINUE;
 	}
 
+	/**
+	 * Check if there is a nonKing piece in the game.
+	 * @param game Game object
+	 * @return true if a nonKingPiece was found, false otherwise
+	 */
 	private boolean kingAlone(Game game) {
-		boolean nonKingFound = false;
 		for (int i = 0; i < 11; i++) {
 			for (int j = 0; j < 11; j++) {
 				edu.colostate.cs.cs414.betterbytes.p4.hnefatafl.game.Cell checkLoc = game.getCell(i, j);
 				if (checkLoc.hasPiece() && checkLoc.getPiece().getType() == "rook") {
-					nonKingFound = true;
+					return true;
 				}
 
 			}
 		}
-		return nonKingFound;
+		return false;
 	}
 
+	/**
+	 * Process a capture between an old game state and a new game state if one happened. Assumes oldGame was
+	 * a previous state of newGame.
+	 * @param oldGame old game state as Game object
+	 * @param newGame new game state as Game object
+	 * @return updated game state
+	 */
 	public Game processCaptures(Game oldGame, Game newGame) {
 		Game gameUpdate = newGame;
 		Cell newLoc = null;
 		Cell oldLoc = null;
+		//Checks to see where the two states differ
 		for(int x = 0; x < 11; x++)
 		{
 			for(int y = 0; y< 11; y++)
@@ -138,8 +166,7 @@ public class RulesEngine {
 		System.out.println(newLoc.getX() + " " + newLoc.getY());
 		//TODO check it works
 		ArrayList<Move> moveHistory = new ArrayList<Move> (Arrays.asList(gameUpdate.getMoves()));		
-		moveHistory.add(new Move(oldLoc.getX(),oldLoc.getY(),newLoc.getX(),newLoc.getY())); 
-		
+		moveHistory.add(new Move(oldLoc.getX(),oldLoc.getY(),newLoc.getX(),newLoc.getY()));
 		//check above
 		if(newLoc.getY() <= 8)
 		{
@@ -148,7 +175,6 @@ public class RulesEngine {
 			if(checkLoc.hasPiece() && checkLoc.getPiece().isWhite() == newLoc.getPiece().isWhite() && captureLoc.hasPiece() && captureLoc.getPiece().isWhite() != newLoc.getPiece().isWhite())
 				//captureLoc.setPiece(null);
 				{
-					System.out.println("Test1");
 					captureLoc.removePiece();
 				}
 		}
@@ -160,7 +186,6 @@ public class RulesEngine {
 			if(checkLoc.hasPiece() && checkLoc.getPiece().isWhite() == newLoc.getPiece().isWhite() && captureLoc.hasPiece() && captureLoc.getPiece().isWhite() != newLoc.getPiece().isWhite())
 				//captureLoc.setPiece(null);
 			{
-				System.out.println("Test2");
 				captureLoc.removePiece();
 			}
 		}
@@ -172,7 +197,6 @@ public class RulesEngine {
 			if(checkLoc.hasPiece() && checkLoc.getPiece().isWhite() == newLoc.getPiece().isWhite() && captureLoc.hasPiece() && captureLoc.getPiece().isWhite() != newLoc.getPiece().isWhite())
 				//captureLoc.setPiece(null);
 			{
-				System.out.println("Test3");
 				captureLoc.removePiece();
 			}
 		}
@@ -186,7 +210,6 @@ public class RulesEngine {
 			if(checkLoc.hasPiece() && checkLoc.getPiece().isWhite() == newLoc.getPiece().isWhite() && captureLoc.hasPiece() && captureLoc.getPiece().isWhite() != newLoc.getPiece().isWhite())
 				//captureLoc.setPiece(null);
 			{
-				System.out.println("Test4");
 				captureLoc.removePiece();
 			}
 		}

3b05b4b741cca4b14c7e0601703c654487871267
LemonOnRye <redhayden@gmail.com>
2018-12-09 10:07:20 -0700

    Added doc to new structure

--- src/edu/colostate/cs/cs414/betterbytes/p4/server/TaskQueue.java
+++ src/edu/colostate/cs/cs414/betterbytes/p4/server/TaskQueue.java
@@ -5,12 +5,23 @@ import java.util.LinkedList;
 import java.util.List;
 
 //Simple Blocking Queue implementation for tasks
+
+/**
+ * Simple Blocking Queue Implementation for tasks
+ * @version 1.0
+ */
 public class TaskQueue {
 	private List<SelectionKey> queue = new LinkedList<SelectionKey>();
 
 	public TaskQueue() {
 	};
 
+	/**
+	 * Enqueue a SelectionKey. If the queue is empty, make a call to notifyAll
+	 * @param t SelectionKey
+	 * @throws InterruptedException
+	 * @see java.lang.Object#notifyAll()
+	 */
 	public synchronized void enqueue(SelectionKey t) throws InterruptedException {
 		int size = queue.size();
 		queue.add(t);
@@ -20,6 +31,12 @@ public class TaskQueue {
 
 	}
 
+	/**
+	 * Attempt to dequeue a SelectionKey. Blocks while the queue is empty.
+	 * @return Dequeue selection key
+	 * @throws InterruptedException
+	 * @see TaskQueue#enqueue(SelectionKey)
+	 */
 	public synchronized SelectionKey dequeue() throws InterruptedException {
 		while (queue.size() == 0) {
 			wait();
